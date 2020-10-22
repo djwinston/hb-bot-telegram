4795c76ba142f59351c17142f4e529fc79a2d74b
Andrew Schastlivcev <andrew.schastlivcev@sparkybit.com>
2020-10-22 23:49:53 +0300

    update state

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const bot = require("./src/controller/telegraf");
-const state = require("./src/state/");
-const dbController = require("./src/controller/db");
-const StateController = require("./src/controller/state");
-const TimeController = require("./src/controller/time");
+const bot = require('./src/controller/telegraf')
+const state = require('./src/state/')
+const dbController = require('./src/controller/db')
+const StateController = require('./src/controller/state')
+const TimeController = require('./src/controller/time')
 
-bot.command("time", (ctx) => ctx.reply(state.now));
-bot.start((ctx) => ctx.reply("Welcome"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on("sticker", (ctx) => ctx.reply("👍"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
-bot.on("text", (ctx) => {
-  const chat_id = ctx.update.message.chat.id;
-  console.log(ctx.update);
-  ctx.reply(chat_id);
-  dbController.writeDB({ chat_id: chat_id });
-});
+bot.command('set', (ctx) => {
+  console.log('set log', ctx.update)
+  const {id, first_name, last_name, username, is_bot} = ctx.update.message.from
+  const user = {id, first_name, last_name, username, is_bot}
+  StateController.update({users: [user]})
+})
+bot.command('time', (ctx) => {
+  console.log('state', state);  
+  ctx.reply(state.now)})
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.on('text', (ctx) => {
+  const chat_id = ctx.update.message.chat.id
+  console.log('ctx.update', ctx.update)
+  ctx.reply(`chat_id=${chat_id}`)
+  StateController.update({ chat_id: chat_id })
+})
 
-bot.launch();
-StateController.init();
+bot.launch()
+StateController.init()
